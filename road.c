f7f8a9baf8c0a76586ca25fad1390636f216b0cb
Cloud Wu <cloudwu@gmail.com>
2022-10-10 10:37:22 +0800

    one-way road

--- road.c
+++ road.c
@@ -4,54 +4,38 @@
 #include <limits.h>
 
 #define MAX_EDGE 3
+#define INVALID_EDGE 0xffff
 
-typedef short edge_t;
+typedef unsigned short edge_t;
 typedef unsigned short length_t;
 
 struct road {
 	length_t len;
-	edge_t e[MAX_EDGE*2];
+	edge_t e[MAX_EDGE];
 };
 
 static inline void
 road_init(struct road r[], int n) {
-	memset(r, 0, sizeof(r[0]) * n);
+	int i,j;
+	for (i=0;i<n;i++) {
+		r[i].len = 0;
+		for (j=0;j<MAX_EDGE;j++) {
+			r[i].e[j] = INVALID_EDGE;
+		}
+	}
 }
 
 static inline void
 road_new(struct road r[], edge_t id, length_t len) {
-	assert(id > 0);
-	r[id-1].len = len;
-}
-
-static inline edge_t *
-edge_get_(struct road r[], edge_t from) {
-	edge_t *e;
-	assert(from != 0);
-	if (from < 0) {
-		from = -from - 1;
-		e =	r[from].e + MAX_EDGE;
-	} else {
-		from--;
-		e =	r[from].e;
-	}
-	return e;
-}
-
-static inline length_t
-edge_len_(struct road r[], edge_t id) {
-	if (id < 0)
-		return r[-id-1].len;
-	else
-		return r[id-1].len;
+	r[id].len = len;
 }
 
 static inline void
-road_connect_(struct road r[], edge_t from, edge_t to) {
-	edge_t *e = edge_get_(r, from);
+road_connect(struct road r[], edge_t from, edge_t to) {
+	edge_t *e = r[from].e;
 	int i;
 	for (i=0;i<MAX_EDGE;i++) {
-		if (e[i] == 0) {
+		if (e[i] == INVALID_EDGE) {
 			e[i] = to;
 			return;
 		}
@@ -59,53 +43,40 @@ road_connect_(struct road r[], edge_t from, edge_t to) {
 	assert(0);
 }
 
-static inline void
-road_connect(struct road r[], edge_t from, edge_t to) {
-	road_connect_(r, from, to);
-	road_connect_(r, -to, -from);
-}
-
 struct route {
 	int len;
 	edge_t id;
 	edge_t from;
-	unsigned short index;
+	edge_t index;
 	unsigned char shortest;
 };
 
-static inline unsigned short
-edge_index_(edge_t id) {
-	assert(id != 0);
-	if (id < 0) {
-		return (-id * 2) - 1;
-	}
-	return id * 2 - 2;
-}
-
 static inline struct route *
 route_fetch(struct route *r, edge_t id) {
-	int index = r[edge_index_(id)].index;
-	if (index == 0) {
+	edge_t index = r[id].index;
+	if (index == INVALID_EDGE) {
 		return NULL;
 	}
-	return &r[index-1];
+	assert(r[index].id == id);
+	return &r[index];
 }
 
 static inline int
 route_insert_(struct road m[], struct route r[], int sz, int n, edge_t from, int len) {
-	edge_t * edge = edge_get_(m, from);
+	edge_t * edge = m[from].e;
 	int i;
-	for (i=0;i<MAX_EDGE && edge[i];i++) {
+	for (i=0;i<MAX_EDGE && edge[i] != INVALID_EDGE;i++) {
 		edge_t e = edge[i];
-		int newlen = len + edge_len_(m, e);
+		int newlen = len + m[e].len;
 		struct route * current = route_fetch(r, e);
 		if (current == NULL) {
 			assert(n < sz);
-			current = &r[n++];
+			current = &r[n];
 			current->len = newlen;
 			current->id = e;
 			current->from = from;
-			r[edge_index_(e)].index = (unsigned short)n;
+			r[e].index = (edge_t)n;
+			++n;
 		} else if (newlen < current->len) {
 			current->len = newlen;
 			current->from = from;			
@@ -135,48 +106,65 @@ find_shortest_(struct route *r, int n) {
 
 static inline void
 route_find(struct road m[], struct route r[], int sz, edge_t from, edge_t to) {
-	memset(r, 0, sizeof(struct route) * sz);
+	int i;
+	for (i=0;i<sz;i++) {
+		r[i].shortest = 0;
+		r[i].index = INVALID_EDGE;
+	}
 	int n = route_insert_(m, r, sz, 0, from, 0);
 	struct route * current;
 	while ((current = find_shortest_(r, n))) {
 		if (current->id == to) {
 			return;
 		}
-		struct road *c = &m[edge_index_(current->id)];
+		struct road *c = &m[current->id];
 		n = route_insert_(m, r, sz, n, current->id, c->len);
 	}
 }
 
 /*
-         1 (100)
+         0,4 (100)
+	+-------------+                 -> 0  |     |  4 <- 
+	|             |                 3     v	    v     5     1 5
+	| 3,7 (30)    | 1,5 (10)        ^     1	    7     ^     | ^      6 <-+ 
+	|             |                 |  2 <-     -> 6  |     +-+      2 --+
 	+-------------+
-	|             |
-	| 4 (30)      | 2 (10)
-	|             |
-	+-------------+
-         3 (20)
+         2,6 (20)
 */
 
 int
 main() {
-	struct road m[4];
-	road_init(m, 4);
-	road_new(m, 1, 100);
-	road_new(m, 2, 10);
-	road_new(m, 3, 20);
-	road_new(m, 4, 30);
+	struct road m[8];
+	road_init(m, 8);
+	road_new(m, 0, 100);
+	road_new(m, 1, 10);
+	road_new(m, 2, 20);
+	road_new(m, 3, 30);
+
+	road_new(m, 4, 100);
+	road_new(m, 5, 10);
+	road_new(m, 6, 20);
+	road_new(m, 7, 30);
+
+	road_connect(m, 0, 1);
 	road_connect(m, 1, 2);
 	road_connect(m, 2, 3);
-	road_connect(m, 3, 4);
-	road_connect(m, 4, 1);
-	road_connect(m, 2, -2);	// turn around
+	road_connect(m, 3, 0);
+
+	road_connect(m, 4, 7);
+	road_connect(m, 7, 6);
+	road_connect(m, 6, 5);
+	road_connect(m, 5, 4);
+	
+	road_connect(m, 1, 5);
+	road_connect(m, 2, 6);	// turn around
 
 	struct route tmp[128];
-	route_find(m, tmp, 128, 1, 3);
+	route_find(m, tmp, 128, 0, 4);
 
 	// print path
-	edge_t from = 1;
-	edge_t to = -1;
+	edge_t from = 0;
+	edge_t to = 4;
 	edge_t checkpoint = to;
 	while (checkpoint != from) {
 		struct route *e = route_fetch(tmp, checkpoint);
